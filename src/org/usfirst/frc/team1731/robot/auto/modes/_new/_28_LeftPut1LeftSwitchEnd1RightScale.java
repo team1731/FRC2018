a28cd486285ec91e7a5799e2bd527ace267820da
daniel <rdanielbuckley@gmail.com>
2018-03-19 18:53:07 -0400

    refactored _new automodes to make them easier to work with

---
+++ src/org/usfirst/frc/team1731/robot/auto/modes/_new/_28_LeftPut1LeftSwitchEnd1RightScale.java
@@ -0,0 +1,14 @@
+package org.usfirst.frc.team1731.robot.auto.modes._new;
+
+import org.usfirst.frc.team1731.robot.auto.AutoModeBase;
+import org.usfirst.frc.team1731.robot.auto.AutoModeEndedException;
+
+public class _28_LeftPut1LeftSwitchEnd1RightScale extends AutoModeBase {
+
+	@Override
+	protected void routine() throws AutoModeEndedException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
